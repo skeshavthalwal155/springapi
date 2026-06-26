d168bb521e054249a2448e98706f44e51d73bac5
skeshavthalwal155 <keshavthalwal155@gmail.com>
2026-06-26 20:50:19 +0530

    build an api

--- src/main/java/com/example/springapi/controller/HomeController.java
+++ src/main/java/com/example/springapi/controller/HomeController.java
@@ -1,16 +1,14 @@
 package com.example.springapi.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
     @RequestMapping("/")
-    public String index(){
-        return "index.html";
-    }
-    @RequestMapping("/hello")
-    public String sayHello(){
-        return "index.html";
+    public String index(Model model){
+        model.addAttribute("name", "Keshav");
+        return "index";
     }
 }
---
+++ src/main/java/com/example/springapi/controller/MessageController.java
@@ -0,0 +1,13 @@
+package com.example.springapi.controller;
+
+import com.example.springapi.entities.Message;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+    @RequestMapping("/hello")
+    public Message sayHello() {
+        return new Message( "Hello World");
+    }
+}
