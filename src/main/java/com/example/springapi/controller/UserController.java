a9092bf0b537d5fb9e3258d94b1b60eec9b46347
skeshavthalwal155 <keshavthalwal155@gmail.com>
2026-06-27 11:39:52 +0530

    data transfer objects

--- src/main/java/com/example/springapi/controller/UserController.java
+++ src/main/java/com/example/springapi/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.example.springapi.controller;
 
+import com.example.springapi.dtos.UserDto;
 import com.example.springapi.entities.User;
 import com.example.springapi.repositories.UserRepository;
 import lombok.AllArgsConstructor;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 
 @RestController
 @AllArgsConstructor
@@ -18,16 +21,20 @@ public class UserController {
     private final UserRepository userRepository;
 
     @GetMapping
-    public Iterable<User> getAllUsers() {
-        return userRepository.findAll();
+    public List<UserDto> getAllUsers() {
+        return userRepository.findAll()
+                .stream()
+                .map(user -> new UserDto(user.getId(), user.getName(), user.getEmail()))
+                .toList();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<User> getUser(@PathVariable Long id){
+    public ResponseEntity<UserDto> getUser(@PathVariable Long id){
         var user =  userRepository.findById(id).orElse(null);
         if (user == null){
             return ResponseEntity.notFound().build();
         }
-        return ResponseEntity.ok(user);
+        var userDto = new UserDto(user.getId(), user.getName(), user.getEmail());
+        return ResponseEntity.ok(userDto);
     }
 }
--- src/main/java/com/example/springapi/repositories/UserRepository.java
+++ src/main/java/com/example/springapi/repositories/UserRepository.java
@@ -1,7 +1,8 @@
 package com.example.springapi.repositories;
 
 import com.example.springapi.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
