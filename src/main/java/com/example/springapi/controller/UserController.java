2eab4e6f40170c6ffa5dee5d85b321acda15c30b
skeshavthalwal155 <keshavthalwal155@gmail.com>
2026-06-27 11:26:53 +0530

    creating dynamic routes

--- src/main/java/com/example/springapi/controller/UserController.java
+++ src/main/java/com/example/springapi/controller/UserController.java
@@ -4,16 +4,24 @@ import com.example.springapi.entities.User;
 import com.example.springapi.repositories.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/users")
 public class UserController {
     private final UserRepository userRepository;
 
-    @GetMapping("/users")
+    @GetMapping
     public Iterable<User> getAllUsers() {
         return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable Long id){
+        return userRepository.findById(id).orElse(null);
     }
 }
