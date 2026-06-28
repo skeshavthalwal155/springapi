f7c930ba0cd2a66f63a5efa89a26ba6507a7bb9e
skeshavthalwal155 <keshavthalwal155@gmail.com>
2026-06-28 11:52:18 +0530

    buliding a curd api for products

--- src/main/java/com/example/springapi/mappers/ProductMapper.java
+++ src/main/java/com/example/springapi/mappers/ProductMapper.java
@@ -4,9 +4,16 @@ import com.example.springapi.dtos.ProductDto;
 import com.example.springapi.entities.Product;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
     @Mapping(target = "categoryId", source = "category.id")
     ProductDto toProductDto(Product product);
+
+    Product toEntity(ProductDto productDto);
+
+    @Mapping(target="id", ignore = true)
+    void update(ProductDto productDto, @MappingTarget Product product);
+
 }
