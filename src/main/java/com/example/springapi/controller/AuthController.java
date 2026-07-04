844b4a2e3155ed37787fe0ceb78d3939eba3bdc7
skeshavthalwal155 <keshavthalwal155@gmail.com>
2026-07-04 11:12:02 +0530

    working with authentication manager

--- src/main/java/com/example/springapi/controller/AuthController.java
+++ src/main/java/com/example/springapi/controller/AuthController.java
@@ -1,42 +1,34 @@
 package com.example.springapi.controller;
 
 import com.example.springapi.dtos.LoginRequest;
-import com.example.springapi.repositories.UserRepository;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 @AllArgsConstructor
 public class AuthController {
-
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
 
     @PostMapping("/login")
     public ResponseEntity<?> login(
             @Valid @RequestBody LoginRequest request
     ){
-        var user = userRepository.findByEmail(request.getEmail()).orElse(null);
-        if (user == null) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(
-                    Map.of("error", "Invalid credentials.")
-            );
-        }
-        if(!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(
-                    Map.of("error", "Invalid credentials.")
-            );
-        }
-        return ResponseEntity.ok().build();
+       authenticationManager.authenticate(
+               new UsernamePasswordAuthenticationToken(
+                       request.getEmail(), request.getPassword()
+               )
+       );
+       return ResponseEntity.ok().build();
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<Void> handleBadCredentialsException() {
+        return ResponseEntity.badRequest().build();
     }
 }
