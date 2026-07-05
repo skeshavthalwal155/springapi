618e56ceb56312ba14f80d913902d9613d037f80
skeshavthalwal155 <keshavthalwal155@gmail.com>
2026-07-05 10:31:40 +0530

    accessing the current user

--- src/main/java/com/example/springapi/controller/AuthController.java
+++ src/main/java/com/example/springapi/controller/AuthController.java
@@ -2,6 +2,9 @@ package com.example.springapi.controller;
 
 import com.example.springapi.dtos.JwtResponse;
 import com.example.springapi.dtos.LoginRequest;
+import com.example.springapi.dtos.UserDto;
+import com.example.springapi.mappers.UserMapper;
+import com.example.springapi.repositories.UserRepository;
 import com.example.springapi.services.JwtService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
@@ -9,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,6 +21,8 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
     @PostMapping("/login")
     public ResponseEntity<JwtResponse> login(
@@ -34,6 +40,19 @@ public class AuthController {
     public boolean validate(@RequestHeader("Authorization") String authHeader){
         var token  = authHeader.replace("Bearer ", "");
         return jwtService.validateToken(token);
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<UserDto> me(){
+        var authentication = SecurityContextHolder.getContext().getAuthentication();
+        var email = (String) authentication.getPrincipal();
+        var user = userRepository.findByEmail(email).orElse(null);
+        if(user == null){
+            return ResponseEntity.notFound().build();
+        }
+        var userDto = userMapper.toDto(user);
+
+        return ResponseEntity.ok(userDto);
     }
 
     @ExceptionHandler(BadCredentialsException.class)
