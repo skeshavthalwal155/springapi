ccf118b3481deb2dd4aca9cbc70c2d75e789154d
skeshavthalwal155 <keshavthalwal155@gmail.com>
2026-07-08 10:55:00 +0530

    refactoring the jwtservice

--- src/main/java/com/example/springapi/services/JwtService.java
+++ src/main/java/com/example/springapi/services/JwtService.java
@@ -1,7 +1,6 @@
 package com.example.springapi.services;
 
 import com.example.springapi.config.JwtConfig;
-import com.example.springapi.entities.Role;
 import com.example.springapi.entities.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
@@ -16,33 +15,33 @@ import java.util.Date;
 public class JwtService {
     private final JwtConfig jwtConfig;
 
-    public String generateAccessToken(User user){
+    public Jwt generateAccessToken(User user){
         return generateToken(user, jwtConfig.getAccessTokenExpiration());
     }
 
-    public String generateRefreshToken(User user){
+    public Jwt generateRefreshToken(User user){
         return generateToken(user, jwtConfig.getRefreshTokenExpiration());
     }
 
-    private String generateToken(User user, long tokenExpiration) {
-        return Jwts.builder()
+    private Jwt generateToken(User user, long tokenExpiration) {
+        var claims =  Jwts.claims()
                 .subject(user.getId().toString())
-                .claim("email", user.getEmail())
-                .claim("name", user.getName())
-                .claim("role", user.getRole())
+                .add("email",user.getEmail())
+                .add("name",user.getName())
+                .add("role",user.getRole())
                 .issuedAt(new Date())
                 .expiration(new Date(System.currentTimeMillis() + 1000 * tokenExpiration))
-                .signWith(jwtConfig.getSecretKey())
-                .compact();
+                .build();
+        return new Jwt(claims, jwtConfig.getSecretKey());
     }
 
-    public boolean validateToken(String token){
-       try{
-           var claims = getClaims(token);
-           return claims.getExpiration().after(new Date());
-       }catch (JwtException ex){
-           return false;
-       }
+    public Jwt parseToken(String token){
+        try{
+            var claims = getClaims(token);
+            return new Jwt(claims, jwtConfig.getSecretKey());
+        }catch(JwtException e){
+            return null;
+        }
     }
 
     private Claims getClaims(String token) {
@@ -51,13 +50,5 @@ public class JwtService {
                 .build()
                 .parseSignedClaims(token)
                 .getPayload();
-
-    }
-
-    public Long getUserIdFromToken(String token){
-        return Long.valueOf(getClaims(token).getSubject());
-    }
-    public Role getRoleFromToken(String token){
-        return Role.valueOf(getClaims(token).get("role", String.class));
     }
 }
