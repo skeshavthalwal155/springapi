3ccbd94506f6c9c94f0802bc9c633edbd3d355bd
skeshavthalwal155 <keshavthalwal155@gmail.com>
2026-07-11 12:19:25 +0530

    handling stripe errors

--- src/main/java/com/example/springapi/dtos/CheckoutResponse.java
+++ src/main/java/com/example/springapi/dtos/CheckoutResponse.java
@@ -5,8 +5,10 @@ import lombok.Data;
 @Data
 public class CheckoutResponse {
     private Long orderId;
+    private String checkoutUrl;
 
-    public CheckoutResponse(Long orderId) {
+    public CheckoutResponse(Long orderId, String url) {
         this.orderId = orderId;
+        this.checkoutUrl = url;
     }
 }
--- src/main/java/com/example/springapi/services/CheckoutService.java
+++ src/main/java/com/example/springapi/services/CheckoutService.java
@@ -7,18 +7,29 @@ import com.example.springapi.exceptions.CartEmptyException;
 import com.example.springapi.exceptions.CartNotFoundException;
 import com.example.springapi.repositories.CartRepository;
 import com.example.springapi.repositories.OrderRepository;
-import lombok.AllArgsConstructor;
+import com.stripe.exception.StripeException;
+import com.stripe.model.checkout.Session;
+import com.stripe.param.checkout.SessionCreateParams;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class CheckoutService {
     private final CartRepository cartRepository;
     private final OrderRepository orderRepository;
     private final CartService cartService;
     private final AuthService authService;
 
-    public CheckoutResponse checkout(CheckoutRequest request) {
+    @Value("${websiteUrl}")
+    private String websiteUrl;
+
+    @Transactional
+    public CheckoutResponse checkout(CheckoutRequest request) throws StripeException {
         var cart = cartRepository.getCartWithItems(request.getCartId()).orElse(null);
         if (cart == null) {
             throw new CartNotFoundException();
@@ -31,8 +42,43 @@ public class CheckoutService {
         var order = Order.fromCart(cart, authService.getCurrentUser());
 
         orderRepository.save(order);
-        cartService.clearCart(cart.getId());
 
-        return new CheckoutResponse(order.getId());
+        try {
+            // create a checkout session
+            var builder = SessionCreateParams.builder()
+                    .setMode(SessionCreateParams.Mode.PAYMENT)
+                    .setSuccessUrl(websiteUrl + "/checkout-success?orderId=" + order.getId())
+                    .setCancelUrl(websiteUrl + "/checkout-cancel.html");
+            order.getItems().forEach(item -> {
+                var lineItem = SessionCreateParams.LineItem.builder()
+                        .setQuantity(Long.valueOf(item.getQuantity()))
+                        .setPriceData(
+                                SessionCreateParams.LineItem.PriceData.builder()
+                                        .setCurrency("usd")
+                                        .setUnitAmountDecimal(
+                                                item.getUnitPrice()
+                                                        .multiply(BigDecimal.valueOf(100)
+                                                        ))
+                                        .setProductData(
+                                                SessionCreateParams.LineItem.PriceData.ProductData.builder()
+                                                        .setName(item.getProduct().getName())
+                                                        .build()
+                                        )
+                                        .build()
+                        )
+                        .build();
+                builder.addLineItem(lineItem);
+            });
+            var session = Session.create(builder.build());
+
+
+            cartService.clearCart(cart.getId());
+
+            return new CheckoutResponse(order.getId(), session.getUrl());
+        } catch (StripeException ex) {
+            System.out.println(ex.getMessage());
+            orderRepository.delete(order);
+            throw ex;
+        }
     }
 }
