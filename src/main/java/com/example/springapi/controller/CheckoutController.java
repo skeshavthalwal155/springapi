9f23420ffdf536c1041f6c48bc8e546510c13b34
skeshavthalwal155 <keshavthalwal155@gmail.com>
2026-07-11 13:06:24 +0530

    building a webhook endpoint

--- src/main/java/com/example/springapi/controller/CheckoutController.java
+++ src/main/java/com/example/springapi/controller/CheckoutController.java
@@ -7,20 +7,55 @@ import com.example.springapi.exceptions.CartEmptyException;
 import com.example.springapi.exceptions.CartNotFoundException;
 import com.example.springapi.exceptions.PaymentException;
 import com.example.springapi.services.CheckoutService;
+import com.stripe.exception.SignatureVerificationException;
+import com.stripe.net.Webhook;
 import jakarta.validation.Valid;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/checkout")
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class CheckoutController {
     private final CheckoutService checkoutService;
+
+    @Value("${stripe.webhookSecretKey}")
+    private String webhookSecretKey;
+
     @PostMapping
     public CheckoutResponse checkout(@Valid @RequestBody CheckoutRequest request) {
         return checkoutService.checkout(request);
+    }
+
+    @PostMapping("/webhook")
+    public ResponseEntity<Void> handleWebHook(
+            @RequestHeader("Stripe-Signature") String signature,
+            @RequestBody String payload
+    ) {
+        try {
+            var event = Webhook.constructEvent(payload, signature, webhookSecretKey);
+            System.out.println(event.getType());
+
+            var stripeObject = event.getDataObjectDeserializer().getObject().orElse(null);
+
+            switch (event.getType()) {
+                case "payment_intent.succeeded" -> {
+
+                }
+                case "payment_intent.failed" -> {
+
+                }
+            }
+
+            return ResponseEntity.ok().build();
+
+        } catch (SignatureVerificationException e) {
+            return ResponseEntity.badRequest().build();
+        }
+
     }
 
     @ExceptionHandler(PaymentException.class)
